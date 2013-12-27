d99812bacf518898baeb0f7e44b64defaa2f6557
akila <akila.aki28@gmail.com>
2013-12-27 13:23:08 +0530

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,8 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -68,53 +70,88 @@ describe User do
     end
   end
 
-       describe "when email address is already taken" do
-         before do
-         user_with_same_email = @user.dup
-         user_with_same_email.email = @user.email.upcase
-         user_with_same_email.save
-         end
-
-         it { should_not be_valid }
+   describe "when email address is already taken" do
+      before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
       end
-       
-      describe "when password is not present" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
-  end
 
-  describe "when password doesn't match confirmation" do
+      it { should_not be_valid }
+    end
+       
+   describe "when password is not present" do
+     before { @user.password = @user.password_confirmation = " " }
+     it { should_not be_valid }
+   end
+
+   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
-  end
+   end
 
-  describe "when password confirmation is nil" do
-    before { @user.password_confirmation = nil }
-    it { should_not be_valid }
-  end
-        describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
+   describe "when password confirmation is nil" do
+     before { @user.password_confirmation = nil }
+     it { should_not be_valid }
+   end
 
-  describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by_email(@user.email) }
+    describe "with a password that's too short" do
+     before { @user.password = @user.password_confirmation = "a" * 5 }
+     it { should be_invalid }
+    end
 
+    describe "return value of authenticate method" do
+     before { @user.save }
+     let(:found_user) { User.find_by_email(@user.email) }
+ 
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
 
     describe "with invalid password" do
-  let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-  it { should_not == user_for_invalid_password }
-  specify { user_for_invalid_password.should be_false }
+    it { should_not == user_for_invalid_password }
+    specify { user_for_invalid_password.should be_false }
   end
-  end
+ end
    
    describe "remember token" do
      before { @user.save }
      its(:remember_token) { should_not be_blank }
     end
-end
+
+   describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+    
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+  end
+ end
